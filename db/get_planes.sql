47eefb04a0e95ae356f462e63304ee13e20bb9c4
unknown <fdsa4321jkl@yahoo.com>
2019-03-28 11:30:55 -0700

    finished massivejs mini proj, added comments to understadability

--- db/get_planes.sql
+++ db/get_planes.sql
@@ -1,1 +1,3 @@
-SELECT * FROM airplanes;
+SELECT * FROM airplanes
+WHERE passenger_count > $1;
+-- $1 refers to ('db').get_planes([25])
--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 const express = require("express");
-
+require('dotenv').config();
+const massive = require('massive');
 const app = express();
+const { SERVER_PORT, CONNECTION_STRING } = process.env;
+const controller = require('./controller')
 
-const { SERVER_PORT } = process.env;
+massive(CONNECTION_STRING).then(database=>{app.set('db',database)
+
+// database.new_planes()
+// .then(planes=>console.log(planes))
+// .catch(err=>console.log(err))
+
+// database.get_planes()
+// .then(planes=>console.log(planes))
+// .catch(err=>console.log(err))
+
+// the COMMENTED CODE ABOVE shows that once the connection is made, it will retrieve NEW_PLANES and GET_PLANES
+
+})
 
 app.use(express.json());
+
+app.get('/api/planes', controller.getPlanes)
+
+// get request is using an ENDPOINT
 
 app.listen(SERVER_PORT, () => {
   console.log(`Server listening on port ${SERVER_PORT}`);
